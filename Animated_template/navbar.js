d55df959ced1cdb38f9d467efc46e918f05c0057
Maya Bachvarova <may13@abv.bg>
2023-06-05 14:05:32 +0300

    Animated template

---
+++ Animated_template/navbar.js
@@ -0,0 +1,6 @@
+function toggleNavbar(collapseID) {
+    console.log(collapseID)
+    document.getElementById(collapseID).classList.toggle("hidden");
+    document.getElementById(collapseID).classList.toggle("block");
+  }
+
